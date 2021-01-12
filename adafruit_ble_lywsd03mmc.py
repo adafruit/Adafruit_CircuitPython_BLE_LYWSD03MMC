b84f95f7deab5201e65b048391d6ee39f7b8a919
Dan Halbert <halbert@halwitz.org>
2021-01-12 10:50:35 -0500

    Initial commit.

---
+++ adafruit_ble_lywsd03mmc.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2021 Dan Halbert, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2021 Adafruit Industries for Adafruit Industries LLC
+#
+# SPDX-License-Identifier: MIT
+"""
+`adafruit_ble_lywsd03mmc`
+================================================================================
+
+BLE Support for Xiaomi LYWSD03MMC Thermometer/Hygrometer
+
+
+* Author(s): Adafruit Industries
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s). Use unordered list & hyperlink rST
+   inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_BLE_LYWSD03MMC.git"
---
+++ docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/ble_lywsd03mmc_simpletest.py
+    :caption: examples/ble_lywsd03mmc_simpletest.py
+    :linenos:
